87b36a51a0145979b233ce51431ab83b05181fd2
Arjan Schrijver <a_gadgetbridge@anymore.nl>
2023-05-15 23:09:02 +0200

    Add simple dashboard view

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/activities/DashboardFragment.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/activities/DashboardFragment.java
@@ -2,9 +2,12 @@ package nodomain.freeyourgadget.gadgetbridge.activities;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import nodomain.freeyourgadget.gadgetbridge.R;
@@ -13,7 +16,13 @@ public class DashboardFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+        View dashboardView = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        setHasOptionsMenu(true);
+        return dashboardView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.dashboard_menu, menu);
     }
 }
